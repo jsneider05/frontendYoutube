061610fc242a9938ffcce7ad4d8eb24df904b5e6
jsneider05 <joan_sneider@hotmail.com>
2019-05-25 12:25:44 -0500

    Comunication with Backend

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,19 @@
+import { UserModel } from './../model/user.model';
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  //private users: Array<UserModel>;
+  /**
+   * getUsers
+   */
+  public getUsers():Observable<UserModel[]> {
+    return this.http.get<UserModel[]>("http://localhost:8080/getUsers");
+  }
 }
